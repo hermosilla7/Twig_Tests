89ee7e225d206170eccbb34f28410eeb92c41663
Mateo Hermosilla <mhermosilla@TRAVELCLICK.NET>
2017-01-02 13:09:16 +0100

    first and second tests

--- 01/index.php
+++ 01/index.php
@@ -7,12 +7,5 @@
 
     echo $twig->render('index.twig', array(
         'name' => 'USCSS Nostromo',
-        'type' => 'Hauler',
-        'owner' => 'Weyland-Yutani',
-        'crew' => array(
-            array('name' => 'Name1', 'role' => 'Role1', 'status' => 'Alive'),
-            array('name' => 'Name2', 'role' => 'Role2', 'status' => 'Unknown', 'notes' => 'Possibly synthetic.'),
-            array('name' => 'Name3', 'role' => 'Role3', 'status' => 'Dead'),
-    )
     ));
 ?>
